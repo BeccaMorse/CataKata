b202eabb898dd80a1758075ab6fffbd31a8c743a
BeccaMorse <beccamorse@gmail.com>
2020-01-02 13:49:38 -0500

    limit selection to 1-46 - negative numbers for greater iterations

--- FibonacciToX/src/com/company/Main.java
+++ FibonacciToX/src/com/company/Main.java
@@ -9,18 +9,30 @@ public class Main {
         //convert to integer
 
         //output the sequence
-        System.out.println("Input a positive integer to generate a Fibonacci Sequence");
+        System.out.println("Input a positive integer between 1 and 46 to generate a Fibonacci Sequence");
         Scanner scanner = new Scanner(System.in);
         int iterations = Integer.parseInt(scanner.nextLine());
 
-        FibonacciGenerator fibonacciGenerator = new FibonacciGenerator(iterations);
+        if (iterations > 0 && iterations < 47) {
 
-        System.out.println("Now Generating " + iterations + " integers in the Fibonacci Sequence...");
+            FibonacciGenerator fibonacciGenerator = new FibonacciGenerator(iterations);
 
-        int[] sequence = fibonacciGenerator.getFibonacciSequence();
+            System.out.println("Now Generating " + iterations + " integers in the Fibonacci Sequence...");
 
-        for (int i = 0 ; i < iterations ; i++) {
-        System.out.print(sequence[i] + " ");
+            int[] sequence = fibonacciGenerator.getFibonacciSequence();
+
+            for (int i = 0; i < iterations; i++) {
+                System.out.print(sequence[i] + " ");
+                if (i % 10 == 0) {
+                    System.out.println();
+                }
+            }
+
+        } else {
+            System.out.println("Please limit your entry to an integer between 1 and 46. Try again.");
+
         }
+
+
     }
 }
