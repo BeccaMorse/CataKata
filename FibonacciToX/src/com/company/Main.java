7029356fb05c89b9cda4ba15492a18680cb670cc
BeccaMorse <beccamorse@gmail.com>
2020-01-02 11:31:06 -0500

    create input and output text in main

--- FibonacciToX/src/com/company/Main.java
+++ FibonacciToX/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,12 @@ public class Main {
         //convert to integer
 
         //output the sequence
+        System.out.println("Input a positive integer to generate a Fibonacci Sequence");
+        Scanner scanner = new Scanner(System.in);
+        int iterations = Integer.parseInt(scanner.nextLine());
+
+
+        System.out.println("Now Generating " + iterations + " integers in the Fibonacci Sequence...");
 
     }
 }
